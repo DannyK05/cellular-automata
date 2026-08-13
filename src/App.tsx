b9b691a761b9845206744643958186cb37b41273
Kolade <olubanjok@gmail.com>
2026-08-13 09:56:05 +0100

    feat: add controls for speed, play and restart; add a constant value for the base matrix and implement logic to generate base new matrix;

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
 import { useEffect, useState } from "react";
 
+const BASE_MATRIX = [
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 1, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0],
+];
 function App() {
-  const [matrix, setMatrix] = useState([
-    [0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0],
-    [0, 0, 1, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0],
-  ]);
+  const [playing, setPlaying] = useState(false);
+  const [speed, setSpeed] = useState(300);
+
+  const [matrix, setMatrix] = useState(BASE_MATRIX);
 
   const automate = (prevMatrix: number[][]) => {
-    const newMatrix = [
-      [0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0],
-    ];
+    const newMatrix = [];
+    //generate new matrix default zeros from matrix dimensions
+    for (let i = 0; i < prevMatrix.length; i++) {
+      newMatrix.push(Array(prevMatrix[0].length).fill(0));
+    }
 
     for (let i = 0; i < prevMatrix.length; i++) {
       const matrixRow = prevMatrix[i];
@@ -51,16 +55,24 @@ function App() {
     return newMatrix;
   };
 
+  const restartSimulation = () => {
+    setMatrix(BASE_MATRIX);
+  };
+
   useEffect(() => {
+    if (!playing) {
+      return;
+    }
     const interval = setInterval(
       () => setMatrix((prev) => automate(prev)),
-      600,
+      speed,
     );
     return () => clearInterval(interval);
-  }, []);
+  }, [speed, playing]);
 
   return (
     <div className="w-full border p-2">
+      <p>Speed: {speed / 1000}sec</p>
       <div
         style={{
           display: "grid",
@@ -79,12 +91,34 @@ function App() {
           )),
         )}
       </div>
-      {/* <button
-        onClick={automate}
-        className="text-green-500 border px-2 cursor-pointer active:bg-green-500 active:text-white"
-      >
-        Next
-      </button> */}
+      <div className="flex items-center justify-center space-x-4 p-2">
+        <button
+          onClick={() => setPlaying((prev) => !prev)}
+          className="text-green-500 border px-2 cursor-pointer active:bg-green-500 active:text-white"
+        >
+          {!playing ? "Play" : "Pause"}
+        </button>
+
+        <button
+          onClick={() => restartSimulation()}
+          className="text-green-500 border px-2 cursor-pointer active:bg-green-500 active:text-white"
+        >
+          Restart
+        </button>
+
+        <button
+          onClick={() => setSpeed((prev) => prev - 50)}
+          className="text-green-500 border px-2 cursor-pointer active:bg-green-500 active:text-white"
+        >
+          Increase speed
+        </button>
+        <button
+          onClick={() => setSpeed((prev) => prev + 50)}
+          className="text-green-500 border px-2 cursor-pointer active:bg-green-500 active:text-white"
+        >
+          Reduce Speed
+        </button>
+      </div>
     </div>
   );
 }
