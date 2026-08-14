acaabeef09e33da8093eb4865e6254845aa4e230
Kolade <olubanjok@gmail.com>
2026-08-14 15:33:46 +0100

    feat: update styling to look like a game console; add toggle for grid outline; add fr per sec speed display

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,51 @@
 import { useEffect, useState } from "react";
 import { generateZeroMatrix } from "./helper";
+import { twMerge } from "tailwind-merge";
+import {
+  FastForward,
+  Grid2X2Icon,
+  PauseIcon,
+  PlayIcon,
+  Rewind,
+  RotateCcw,
+  Square,
+} from "lucide-react";
 
-// const BASE_MATRIX = [
-//   [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-//   [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-//   [1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-//   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-//   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-//   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-//   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-//   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-//   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-//   [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
-//   [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1],
-// ];
 function App() {
-  const BASE_MATRIX = generateZeroMatrix(16, 16);
+  const BASE_MATRIX = generateZeroMatrix(12, 12);
   const [playing, setPlaying] = useState(false);
   const [speed, setSpeed] = useState(300);
-
+  const [borderOn, setBorderOn] = useState(true);
   const [matrix, setMatrix] = useState(BASE_MATRIX);
+
+  const controls = [
+    {
+      effect: () => setPlaying((prev) => !prev),
+      children: () => (!playing ? <PlayIcon /> : <PauseIcon />),
+    },
+    {
+      effect: () => restartSimulation(),
+      children: () => <RotateCcw />,
+    },
+    {
+      effect: () => setSpeed((prev) => prev + 50),
+      children: () => <Rewind />,
+    },
+    {
+      effect: () =>
+        setSpeed((prev) => {
+          if (prev > 50) {
+            prev = prev - 50;
+          }
+          return prev;
+        }),
+      children: () => <FastForward />,
+    },
+    {
+      effect: () => setBorderOn((prev) => !prev),
+      children: () => (!borderOn ? <Grid2X2Icon /> : <Square />),
+    },
+  ];
 
   const automate = (prevMatrix: number[][]) => {
     //generate new matrix default zeros from matrix dimensions
@@ -142,8 +168,14 @@ function App() {
   }, [speed, playing]);
 
   return (
-    <div className="w-full border p-2">
-      <p>Speed: {speed / 1000}sec</p>
+    <div className="w-full border py-2 px-4 bg-[#6A46CA] rounded-b-xl lg:w-3/5">
+      <h2 className="text-center press-start text-[#CD8329]">
+        Conway Game of Life Simulator
+      </h2>
+      <div className="w-fit flex items-center space-x-4 p-1 mb-2 border border-purple-800 text-[8px] text-green-900 font-bold press-start bg-green-200">
+        <p>Speed: {(1 / (speed / 1000)).toFixed(2)}frm/secs</p>
+        {/* <p>Generation: {speed / 1000}sec</p> */}
+      </div>
       <div
         style={{
           display: "grid",
@@ -158,42 +190,27 @@ function App() {
                 setMatrix((prev) => toggleCell(prev, columnIndex, rowIndex))
               }
               key={`${rowIndex}-${columnIndex}`}
-              className={
-                cell == 0
-                  ? "bg-red-500 h-6 w-full cursor-pointer border"
-                  : "bg-green-500 h-6 w-full cursor-pointer border"
-              }
+              className={twMerge(
+                !playing && cell === 0 && "bg-red-200",
+                playing && cell === 0 && "bg-green-200",
+                cell == 1 && "bg-green-800",
+                borderOn && "border",
+                "h-6 w-full cursor-pointer border-green-900 hover:lg:bg-green-600",
+              )}
             ></span>
           )),
         )}
       </div>
-      <div className="flex items-center justify-center space-x-4 p-2">
-        <button
-          onClick={() => setPlaying((prev) => !prev)}
-          className="text-green-500 border px-2 cursor-pointer active:bg-green-500 active:text-white"
-        >
-          {!playing ? "Play" : "Pause"}
-        </button>
-
-        <button
-          onClick={() => restartSimulation()}
-          className="text-green-500 border px-2 cursor-pointer active:bg-green-500 active:text-white"
-        >
-          Restart
-        </button>
-
-        <button
-          onClick={() => setSpeed((prev) => prev - 50)}
-          className="text-green-500 border px-2 cursor-pointer active:bg-green-500 active:text-white"
-        >
-          Increase speed
-        </button>
-        <button
-          onClick={() => setSpeed((prev) => prev + 50)}
-          className="text-green-500 border px-2 cursor-pointer active:bg-green-500 active:text-white"
-        >
-          Reduce Speed
-        </button>
+      <sub className="press-start text-[#CD8329]">Kxlade</sub>
+      <div className="flex items-center justify-center space-x-4 px-2 py-6">
+        {controls.map(({ children, effect }) => (
+          <button
+            onClick={effect}
+            className="text-[#CD8329] bg-purple-800 shadow-lg rounded-lg py-1 px-2 cursor-pointer lg:hover:bg-purple-700 active:bg-purple-600 active:text-white"
+          >
+            {children()}
+          </button>
+        ))}
       </div>
     </div>
   );
